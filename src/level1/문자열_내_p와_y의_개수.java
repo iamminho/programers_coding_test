5fe3faab4f744f87fb5270c73d969173f6c7478c
iamminho <minho100227@kakao.com>
2023-03-23 10:47:09 +0900

    숫자 문자열과 영단어 -level1

--- src/level1/문자열_내_p와_y의_개수.java
+++ src/level1/문자열_내_p와_y의_개수.java
@@ -1,4 +1,34 @@
 package level1;
 
 public class 문자열_내_p와_y의_개수 {
+    /*
+    * s의 모든 문자를 소문자로 치환
+    * s의 모든 p문자를 ""로 치환
+    * 원래 길이와 치환한 길이의 차를 구해줌
+    * y문자도 위와같이 구해줌
+    * 결과적으로 p와 y의 개수를 구해서 비교 -> 둘다 0, 같으면 -> true / 다르면 false
+    *
+    * 필요한 기능
+    * 특정 문자를 ""로 치환해서 원래 길이와의 차를 구해주는 기능
+    * */
+
+    public static void main(String[] args) {
+        String s = "ozdyfetf";
+        System.out.println("result = " + solution(s));
+    }
+
+    private static boolean solution(String s) {
+        String lowerCase = s.toLowerCase();
+        int pNumber = getStrNumber("p", lowerCase);
+        int yNumber = getStrNumber("y", lowerCase);
+
+        return pNumber == yNumber;
+    }
+
+    private static int getStrNumber(String v, String s) {
+        String replaced = s.replace(v, "");
+        return s.length() - replaced.length();
+    }
+
+
 }
---
+++ src/level1/숫자_문자열과_영단어.java
@@ -0,0 +1,23 @@
+package level1;
+
+public class 숫자_문자열과_영단어 {
+    /*
+    * zero부터 nine까지 replace를 시도한다.
+    * */
+
+    private static String[] numbers = new String[] {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    public static void main(String[] args) {
+        String s = "one4seveneight";
+        int result = solution(s);
+        System.out.println("result = " + result);
+
+    }
+    public static int solution(String s) {
+        for (int i = 0; i < numbers.length; i++) {
+            s = s.replace(numbers[i], Integer.toString(i));
+        }
+
+        return Integer.parseInt(s);
+    }
+}
