6419918e3962087a5768a94ed5d58b546d39e13d
iamminho <minho100227@kakao.com>
2023-03-23 11:20:16 +0900

    문자열 다루기 기본 -level1

---
+++ src/level1/문자열_다루기_기본.java
@@ -0,0 +1,21 @@
+package level1;
+
+public class 문자열_다루기_기본 {
+    public static void main(String[] args) {
+        String s = "1234";
+        boolean result = solution(s);
+        System.out.println("result = " + result);
+    }
+
+    public static boolean solution(String s) {
+        return(isNumber(s));
+    }
+
+    public static boolean isNumber(String s) {
+        String regex = "[0-9]{4}|[0-9]{6}";
+        if (s.matches(regex)) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/level1/신규_아이디_추천.java
@@ -0,0 +1,4 @@
+package level1;
+
+public class 신규_아이디_추천 {
+}
