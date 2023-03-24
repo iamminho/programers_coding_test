ee308ad835cb577d8bdcdef01c53f4f1cad57bae
iamminho <minho100227@kakao.com>
2023-03-24 09:49:05 +0900

    쿼드압축 후 개수 세기 -level2

---
+++ src/level2/쿼드압축_후_개수_세기.java
@@ -0,0 +1,52 @@
+package level2;
+
+public class 쿼드압축_후_개수_세기 {
+    private static class Count {
+        private final int zero;
+        private final int one;
+        public Count(int zero, int one) {
+            this.zero = zero;
+            this.one = one;
+        }
+
+        public Count add(Count other) {
+            return new Count(zero + other.zero, one + other.one);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 1, 0, 0},
+                {1, 0, 0, 0},
+                {1, 0, 0, 1},
+                {1, 1, 1, 1}
+        };
+
+        int[] result = solution(arr);
+        for (int v : result) {
+            System.out.println("v = " + v);
+        }
+    }
+
+    private static int[] solution(int[][] arr) {
+        Count count = count(0, 0, arr.length, arr);
+        return new int[]{count.zero, count.one};
+    }
+
+    private static Count count(int offsetX, int offsetY, int size, int[][] arr) {
+        int h = size / 2;
+        for (int x = offsetX; x < offsetX + size; x++) {
+            for (int y = offsetY; y < offsetY + size; y++) {
+                if (arr[x][y] != arr[offsetX][offsetY]) {
+                    return count(offsetX, offsetY, h, arr)
+                            .add(count(offsetX + h, offsetY, h, arr))
+                            .add(count(offsetX, offsetY + h, h, arr))
+                            .add(count(offsetX + h, offsetY + h, h, arr));
+                }
+            }
+        }
+        if (arr[offsetX][offsetY] == 1) return new Count(0, 1);
+        return new Count(1, 0);
+    }
+
+}
