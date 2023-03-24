fa28c4d03f3ac28f21a947b9457a3a7419cbe162
iamminho <minho100227@kakao.com>
2023-03-24 17:52:52 +0900

    하노이의 탑 -level3

---
+++ src/level3/하노이의_탑.java
@@ -0,0 +1,36 @@
+package level3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class 하노이의_탑 {
+    public static void main(String[] args) {
+        int[][] result = solution(3);
+        for (int[] ints : result) {
+
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] solution(int n) {
+        List<int[]> process = new ArrayList<>();
+        hanoi(n, 1, 3, process);
+        return process.toArray(new int[0][]);
+    }
+    private static void hanoi(int n, int from, int to, List<int[]> process) {
+        if (n == 1) {
+            process.add(new int[]{from, to});
+            return;
+        }
+
+        int empty = 6 - from - to;
+
+        hanoi(n - 1, from, empty, process);
+        hanoi(1, from, to, process);
+        hanoi(n - 1, empty, to, process);
+    }
+}
